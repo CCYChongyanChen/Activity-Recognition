739b66c9d9b9cb53def3b5336da13912113886d5
Tim <tiansky.yao95@gmail.com>
2018-11-24 00:31:36 -0600

    Added multi video input to extract_frames

--- Final_Project/tf-pose-estimation/extract_frames.py
+++ Final_Project/tf-pose-estimation/extract_frames.py
@@ -93,23 +93,9 @@ def Four_classifications(X,Y):
 #    print("MLP Split train/test Accuracy:",MLP_s_acc)!
     
     return True
-def extract_frames(csv_path,video_path,output_path):
+def extract_frames(csv_paths,video_paths,output_path):
     fps_time = 0
-    
-    # Read label time ranges
-    rawdata = pd.read_csv(csv_path, header=0)
-    rawdata.columns = ['start_time', 'end_time', 'label']
-    
-    cap = cv2.VideoCapture(video_path)
-    #cap.set(cv2.CAP_PROP_POS_MSEC, 170*1000)
-    #cap.set(cv2.CAP_PROP_POS_MSEC, 194*1000)
-    
-    # Setup model
-    w = 243
-    h = 432
-    showBG = True
-    e = TfPoseEstimator(get_graph_path('mobilenet_thin'), target_size=(w, h))
-    
+
     BODY_PARTS = { "Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
                     "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
                     "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
@@ -118,137 +104,157 @@ def extract_frames(csv_path,video_path,output_path):
     BODY_PARTS_INTEREST = { "Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
                     "LShoulder": 5, "LElbow": 6, "LWrist": 7, "REye": 14,
                     "LEye": 15}
-    
-    # Save a pose to csv every n frames
-    counter = 0
-    ma_counter = 0
-    n = 5
-    ma_length = 5 # Length of the moving average
-    part_weight = 1.0/ma_length # Weight of a part in the moving average
-    last_label = ''
-    
-    # Moving average of all body part locations; Used for filtering out jitters of the posenet
-    bodypart_array_new = [] # Current set of bodyparts
-    bodypart_array_ma = [] # Moving average set of bodyparts
-    ma_bodyparts_array = [] # Sets of bodyparts for computing the moving average
-    bodypart_to_del_idx = ma_length - 1; # Index of the set of bodyparts to overwrite in ma_bodyparts_array
-    # Initialize arrays
-    for i in range(0,10):
-        bodypart_array_new.insert(i, BodyPart(0,0,0.5,0.5,0))
-        bodypart_array_ma.insert(i, BodyPart(0,0,0,0,0)) # Must be all zeros
-    for i in range(0,ma_length):
-        ma_bodyparts_array.insert(i, bodypart_array_ma)
 
-    bodypart_locations = np.zeros(2*10)
+    # Setup posenet model
+    w = 243
+    h = 432
+    showBG = True
+    e = TfPoseEstimator(get_graph_path('mobilenet_thin'), target_size=(w, h))
+
+    # Setup output features table
     out_data = pd.DataFrame(columns=['nose_x', 'nose_y', 'neck_x', 'neck_y', 'rshoulder_x', 'rshoulder_y', 
          'relbow_x', 'relbow_y', 'rwrist_x', 'rwrist_y', 'lshoulder_x', 'lshoulder_y', 'lelbow_x', 'lelbow_y', 
         'lwrist_x', 'lwrist_y', 'reye_x', 'reye_y', 'leye_x', 'leye_y', 'label'])
-    
-    if cap.isOpened() is False:
-        print("Error opening video stream or file")
 
+    # Open up an output display window
     cv2.namedWindow('output',cv2.WINDOW_NORMAL) 
     cv2.resizeWindow('output', 576,1024)
-    
-    while cap.isOpened():
-        ret_val, image = cap.read()
-    
-        timestamp = cap.get(cv2.CAP_PROP_POS_MSEC)/1000.0
-        #if timestamp > 575:
-        #    break
-    
-        label = rawdata['label'][(timestamp > rawdata['start_time']) & (timestamp < rawdata['end_time'])]
-        try:
-            humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=2)
-        except:
-            break
-        if len(humans) > 0:
-            humans = [humans[0]]
 
-            # Extract body part of interest
-            # If a part is not detected, use the part's last location
-            for idx, key in enumerate(BODY_PARTS_INTEREST):
-                try:
-                    bodypart_array_new[idx] = humans[0].body_parts[BODY_PARTS[key]]
-                except:
-                    bodypart_array_new[idx] = bodypart_array_ma[idx]
-    
-            # Perform moving average
-            if ma_counter == ma_length:
-                bodypart_array_delete = ma_bodyparts_array[bodypart_to_del_idx]
-                for idx, part in enumerate(bodypart_array_ma):
-                    part.x = part.x + bodypart_array_new[idx].x*part_weight - \
-                        bodypart_array_delete[idx].x*part_weight
-                    part.y = part.y + bodypart_array_new[idx].y*part_weight - \
-                        bodypart_array_delete[idx].y*part_weight
-                    bodypart_array_ma[idx] = part
-                ma_bodyparts_array[bodypart_to_del_idx] = copy.deepcopy(bodypart_array_new)
-                bodypart_to_del_idx = (bodypart_to_del_idx - 1)%ma_length
-                bodypart_array = bodypart_array_ma
-            else:
-                # We have not seen enough samples yet for the moving average
-                # Just use the current sample as the output of the moving average filter
-                bodypart_array = bodypart_array_new
-                ma_counter = ma_counter + 1
-                ma_bodyparts_array[ma_length - ma_counter] = copy.deepcopy(bodypart_array_new)
-                for idx, part in enumerate(bodypart_array_ma):
-                    part.x += bodypart_array_new[idx].x*part_weight
-                    part.y += bodypart_array_new[idx].y*part_weight
-                    bodypart_array_ma[idx] = part
+    # Start extracting features for every video
+    for idx, (csv_path, video_path) in enumerate(zip(csv_paths, video_paths)):
+        # Read label time ranges
+        rawdata = pd.read_csv(csv_path, header=0)
+        rawdata.columns = ['start_time', 'end_time', 'label']
+        
+        cap = cv2.VideoCapture(video_path)
+        #cap.set(cv2.CAP_PROP_POS_MSEC, 350*1000)
+        
+        # Save a pose to csv every n frames
+        counter = 0
+        n = 5
+        last_label = ''
 
-            # Update the humans array with the moving average data
-            humans[0].body_parts = {}
-            for idx, (key, part) in enumerate(zip(BODY_PARTS_INTEREST, bodypart_array)):
-                humans[0].body_parts[BODY_PARTS[key]] = part
+        # Moving average filter settings
+        ma_length = 5 # Length of the moving average
+        part_weight = 1.0/ma_length # Weight of a part in the moving average
+        ma_counter = 0
+        
+        # Moving average of all body part locations; Used for filtering out jitters of the posenet
+        bodypart_array_new = [] # Current set of bodyparts
+        bodypart_array_ma = [] # Moving average set of bodyparts
+        ma_bodyparts_array = [] # Sets of bodyparts for computing the moving average
+        bodypart_to_del_idx = ma_length - 1; # Index of the set of bodyparts to overwrite in ma_bodyparts_array
+        # Initialize arrays
+        for i in range(0,10):
+            bodypart_array_new.insert(i, BodyPart(0,0,0.5,0.5,0))
+            bodypart_array_ma.insert(i, BodyPart(0,0,0,0,0)) # Must be all zeros
+        for i in range(0,ma_length):
+            ma_bodyparts_array.insert(i, bodypart_array_ma)
 
-            # Draw the skeleton (only the parts of interest)
-            if not showBG:
-                image = np.zeros(image.shape)
-            image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
-    
-            # Generate feature vector
-            neck = bodypart_array[BODY_PARTS['Neck']]
-            for idx, part in enumerate(bodypart_array):
-                bodypart_locations[idx*2] = part.x - neck.x
-                bodypart_locations[idx*2+1] = part.y - neck.y
+        bodypart_locations = np.zeros(2*10)
+        
+        if cap.isOpened() is False:
+            print("Error opening video stream or file")
+        
+        while cap.isOpened():
+            ret_val, image = cap.read()
+        
+            timestamp = cap.get(cv2.CAP_PROP_POS_MSEC)/1000.0
+            #if timestamp > 575:
+            #    break
+        
+            label = rawdata['label'][(timestamp > rawdata['start_time']) & (timestamp < rawdata['end_time'])]
+            try:
+                humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=2)
+            except:
+                break
+            if len(humans) > 0:
+                humans = [humans[0]]
 
-            # Add label and append to feature matrix
-            if label.size != 0:
-                # Check if the current label has persisted over the past n frames
-                if last_label != label.values[0]:
-                    counter = 0
+                # Extract body part of interest
+                # If a part is not detected, use the part's last location
+                for idx, key in enumerate(BODY_PARTS_INTEREST):
+                    try:
+                        bodypart_array_new[idx] = humans[0].body_parts[BODY_PARTS[key]]
+                    except:
+                        bodypart_array_new[idx] = bodypart_array_ma[idx]
+        
+                # Perform moving average
+                if ma_counter == ma_length:
+                    bodypart_array_delete = ma_bodyparts_array[bodypart_to_del_idx]
+                    for idx, part in enumerate(bodypart_array_ma):
+                        part.x = part.x + bodypart_array_new[idx].x*part_weight - \
+                            bodypart_array_delete[idx].x*part_weight
+                        part.y = part.y + bodypart_array_new[idx].y*part_weight - \
+                            bodypart_array_delete[idx].y*part_weight
+                        bodypart_array_ma[idx] = part
+                    ma_bodyparts_array[bodypart_to_del_idx] = copy.deepcopy(bodypart_array_new)
+                    bodypart_to_del_idx = (bodypart_to_del_idx - 1)%ma_length
+                    bodypart_array = bodypart_array_ma
                 else:
-                    if counter == n:
-                        print(label.values[0])
-                        out_data.loc[len(out_data), 'nose_x':'leye_y'] = bodypart_locations
-                        out_data.loc[len(out_data)-1, 'label'] = label.values[0]
+                    # We have not seen enough samples yet for the moving average
+                    # Just use the current sample as the output of the moving average filter
+                    bodypart_array = bodypart_array_new
+                    ma_counter = ma_counter + 1
+                    ma_bodyparts_array[ma_length - ma_counter] = copy.deepcopy(bodypart_array_new)
+                    for idx, part in enumerate(bodypart_array_ma):
+                        part.x += bodypart_array_new[idx].x*part_weight
+                        part.y += bodypart_array_new[idx].y*part_weight
+                        bodypart_array_ma[idx] = part
+
+                # Update the humans array with the moving average data
+                humans[0].body_parts = {}
+                for idx, (key, part) in enumerate(zip(BODY_PARTS_INTEREST, bodypart_array)):
+                    humans[0].body_parts[BODY_PARTS[key]] = part
+
+                # Draw the skeleton (only the parts of interest)
+                if not showBG:
+                    image = np.zeros(image.shape)
+                image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
+        
+                # Generate feature vector
+                neck = bodypart_array[BODY_PARTS['Neck']]
+                for idx, part in enumerate(bodypart_array):
+                    bodypart_locations[idx*2] = part.x - neck.x
+                    bodypart_locations[idx*2+1] = part.y - neck.y
+
+                # Add label and append to feature matrix
+                if label.size != 0:
+                    cv2.putText(image,"Label: "+str(label.values[0]), (10, 70),  cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                    # Check if the current label has persisted over the past n frames
+                    if last_label != label.values[0]:
                         counter = 0
                     else:
-                        if label.values[0] == 'steering':
-                            counter += 0.5
-                            # We have too many steering labels (about twice as much)
+                        if counter == n:
+                            print(label.values[0])
+                            out_data.loc[len(out_data), 'nose_x':'leye_y'] = bodypart_locations
+                            out_data.loc[len(out_data)-1, 'label'] = label.values[0]
+                            counter = 0
                         else:
-                            counter += 1
-                last_label = label.values[0]
+                            if label.values[0] == 'steering':
+                                counter += 0.5
+                                # We have too many steering labels (about twice as much)
+                            else:
+                                counter += 1
+                    last_label = label.values[0]
 
-        cv2.putText(image, "FPS: %f" % (1.0 / (time.time() - fps_time)), (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        try:
-            cv2.putText(image,"Prediction:"+str(predic), (10, 20),  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        except:
-            cv2.putText(image,"Prediction:", (10, 70),  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        cv2.imshow('output', image)
-        fps_time = time.time()
-        if cv2.waitKey(1) == 27:
-            break
+            cv2.putText(image, "FPS: %f" % (1.0 / (time.time() - fps_time)), (10, 20),  cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv2.imshow('output', image)
+            fps_time = time.time()
+            if cv2.waitKey(1) == 27:
+                break
+
+        cap.release()
     
+    # Save features to output csv
     out_data.to_csv(output_path,index=False)
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    csv_path='../../../Work/videos/RealData/Chongyan.csv'
-    video_path='../../../Work/videos/RealData/Chongyan.MOV'
-    output_path='../../../Work/Project/Chongyan_poses.csv'
-    #extract_frames(csv_path,video_path,output_path)
+    csv_paths=['../../../Work/videos/RealData/Chongyan.csv', '../../../Work/videos/RealData/Tim.csv']
+    video_paths=['../../../Work/videos/RealData/Chongyan.MOV', '../../../Work/videos/RealData/Tim.MOV']
+    output_path='../../../Work/Project/features.csv'
+    extract_frames(csv_paths,video_paths,output_path)
 
     raw_poses = pd.read_csv(output_path, header=0)
     raw_poses.columns = ['nose_x', 'nose_y', 'neck_x', 'neck_y', 'rshoulder_x', 'rshoulder_y', 
