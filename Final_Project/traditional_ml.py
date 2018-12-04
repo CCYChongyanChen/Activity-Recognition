Fixed incorrect wrist to nose calculation; Added all subject data

Final_Project/traditional_ml.py
@@ -19,6 +19,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 
+BODY_PARTS_TO_USE = ['nose_x', 'nose_y', 'relbow_x', 'relbow_y', 'rwrist_x', 'rwrist_y', 'lelbow_x', 'lelbow_y', 
+        'lwrist_x', 'lwrist_y']
+
+SECONDARY_FEATURES = ['lr_wrist_dist', 'r_wrist_to_nose_dist', 'r_wrist_velocity_y']
+
+LABEL_COL = ['label']
+
 def Classifications_CV(X,Y):
     print(len(Y))
     confmatrix = np.zeros((7,7))
@@ -58,7 +65,7 @@ def Classifications_CV(X,Y):
     print("GNB cross-validation Accuracy:",np.mean(GNB_average_acc))
     print("RF cross-validation Accuracy:",np.mean(RF_average_acc))
     print("MLP cross-validation Accuracy:",np.mean( MLP_average_acc))
-    print(confmatrix)
+    #print(confmatrix)
 
 def Classifications_LOO(X_train,Y_train, X_test, Y_test):
     confmatrix = np.zeros((7,7))
@@ -96,16 +103,31 @@ def Classifications_LOO(X_train,Y_train, X_test, Y_test):
     print("GNB cross-validation Accuracy:",np.mean(GNB_average_acc))
     print("RF cross-validation Accuracy:",np.mean(RF_average_acc))
     print("MLP cross-validation Accuracy:",np.mean( MLP_average_acc))
-    print(confmatrix)
+    #print(confmatrix)
+
+    return np.mean(RF_average_acc), np.mean( MLP_average_acc)
+
+def Classifications_MLP(X_train,Y_train, X_test, Y_test):
+    confmatrix = np.zeros((7,7))
+    MLP_average_acc=[]
+
+    for i in range(10,100):
+        clf = MLPClassifier(solver='lbfgs', alpha=1e-3, hidden_layer_sizes=(i, i), 
+                        random_state=1, verbose=False)
+        clf.fit(X_train, Y_train)
+        predictions = clf.predict(X_test)
+        #print(clf.classes_)
+        #print(confusion_matrix(Y_test, predictions))
+        
+        #confmatrix+= confusion_matrix(Y_test, predictions) 
+        #joblib.dump(clf,'clf.model')     
+
+        MLP_acc = clf.score(X_test,Y_test)
+        MLP_average_acc.append(MLP_acc)
+            
+        print("Size:", i, "MLP cross-validation Accuracy:",MLP_acc)
 
 def extract_test_train(feature_csvs, test_idx):
-    BODY_PARTS_TO_USE = ['nose_x', 'nose_y', 'relbow_x', 'relbow_y', 'rwrist_x', 'rwrist_y', 'lelbow_x', 'lelbow_y', 
-        'lwrist_x', 'lwrist_y']
-
-    SECONDARY_FEATURES = ['lr_wrist_dist', 'r_wrist_to_nose_dist', 'r_wrist_velocity_y']
-
-    LABEL_COL = ['label']
-
     WINDOW_SIZE = 30 # MAX Window size in number of frames (most samples will be max window size)
     WINDOW_OVERLAP = 15 # Window overlap in number of frames
     FRAME_TIME = 1.0/15.0 # The time of one frame
@@ -131,8 +153,10 @@ def extract_test_train(feature_csvs, test_idx):
             pow(raw_poses['lwrist_y'] - raw_poses['rwrist_y'],2), 0.5)
 
         # r_wrist_to_nose_dist
-        r_wrist_to_nose_dist = pow(pow(raw_poses['lwrist_x'] - raw_poses['rwrist_x'],2) + 
-            pow(raw_poses['lwrist_y'] - raw_poses['rwrist_y'],2), 0.5)
+        r_wrist_to_nose_dist = pow(pow(raw_poses['rwrist_x'] - raw_poses['nose_x'],2) + 
+            pow(raw_poses['rwrist_y'] - raw_poses['nose_y'],2), 0.5)
+        # r_wrist_to_nose_dist = pow(pow(raw_poses['lwrist_x'] - raw_poses['rwrist_x'],2) + 
+        #     pow(raw_poses['lwrist_y'] - raw_poses['rwrist_y'],2), 0.5)
 
         # Perform windowing and compute time dependent data like velocity
         current_idx = 0
@@ -166,7 +190,7 @@ def extract_test_train(feature_csvs, test_idx):
             out_idx = len(out_data)
             out_data.loc[out_idx, BODY_PARTS_TO_USE] = curwnd_raw_poses_cropped.tail(1).values[0]
             out_data.loc[out_idx, LABEL_COL] = current_activity
-            out_data.loc[out_idx, SECONDARY_FEATURES[0]] = max(curwnd_lr_wrist_dist)
+            out_data.loc[out_idx, SECONDARY_FEATURES[0]] = curwnd_lr_wrist_dist.max()
             out_data.loc[out_idx, SECONDARY_FEATURES[1]] = curwnd_r_wrist_to_nose_dist.tail(1).values[0]
             out_data.loc[out_idx, SECONDARY_FEATURES[2]] = r_wrist_velocity_y
 
@@ -178,52 +202,108 @@ def extract_test_train(feature_csvs, test_idx):
         else:
             train = train.append(out_data)
 
-    return test, train
+        # print(out_data[out_data['label']=='eating_to_mouth']['r_wrist_to_nose_dist'].mean())
+        # print(out_data[out_data['label']=='eating_to_lap']['r_wrist_to_nose_dist'].mean())
+        # print(out_data[out_data['label']=='texting']['r_wrist_to_nose_dist'].mean())
+        # print(out_data[out_data['label']=='reading']['r_wrist_to_nose_dist'].mean())
+        # print(out_data[out_data['label']=='steering']['r_wrist_to_nose_dist'].mean())
+        # print('\n')
 
+    return test, train
+def data_augment_eating(orig_data, count):
+    new_data = pd.DataFrame(0, index=np.arange(count*2), columns=BODY_PARTS_TO_USE+SECONDARY_FEATURES+LABEL_COL)
+    # Do the to_mouth feature first
+    data_cropped = orig_data[orig_data['label']=='eating_to_mouth']
+    for BODY_PART in BODY_PARTS_TO_USE:
+        mean = data_cropped[BODY_PART].mean()
+        std = data_cropped[BODY_PART].std()/2
+
+        new_data.loc[0:count-1, BODY_PART] = std*np.random.randn(count,1) + mean
+
+    to_mouth_mean = data_cropped['r_wrist_velocity_y'].mean()
+    to_mouth_std = data_cropped['r_wrist_velocity_y'].std()/2
+    new_data.loc[0:count-1, 'r_wrist_velocity_y'] = to_mouth_std*np.random.randn(count,1) + to_mouth_mean
+    new_data.loc[0:count-1, 'label'] = 'eating_to_mouth'
+
+    # Do the to_lap feature
+    data_cropped = orig_data[orig_data['label']=='eating_to_lap']
+    for BODY_PART in BODY_PARTS_TO_USE:
+        mean = data_cropped[BODY_PART].mean()
+        std = data_cropped[BODY_PART].std()/2
+
+        new_data.loc[count:2*count-1, BODY_PART] = std*np.random.randn(count,1) + mean
+    to_lap_mean = data_cropped['r_wrist_velocity_y'].mean()
+    to_lap_std = data_cropped['r_wrist_velocity_y'].std()/2
+    new_data.loc[count:2*count-1, 'r_wrist_velocity_y'] = to_lap_std*np.random.randn(count,1) + to_lap_mean
+    new_data.loc[count:2*count-1, 'label'] = 'eating_to_lap'
+
+    # Compute secondary features that are NOT time dependent
+    # lr_wrist_dist
+    new_data['lr_wrist_dist'] = pow(pow(new_data['lwrist_x'] - new_data['rwrist_x'],2) + 
+        pow(new_data['lwrist_y'] - new_data['rwrist_y'],2), 0.5)
+
+    # r_wrist_to_nose_dist
+    new_data['r_wrist_to_nose_dist'] = pow(pow(new_data['rwrist_x'] - new_data['nose_x'],2) + 
+        pow(new_data['rwrist_y'] - new_data['nose_y'],2), 0.5)
+    # new_data['r_wrist_to_nose_dist'] = pow(pow(new_data['lwrist_x'] - new_data['rwrist_x'],2) + 
+    #     pow(new_data['lwrist_y'] - new_data['rwrist_y'],2), 0.5)
+
+    return orig_data.append(new_data)
 
 if __name__ == '__main__':
-    csv_paths=['../../../Work/videos/RealData/Chongyan.csv', '../../../Work/videos/RealData/Ershad.csv', '../../../Work/videos/RealData/Tim.csv']
-    video_paths=['../../../Work/videos/RealData/Chongyan.MOV', '../../../Work/videos/RealData/Ershad.MOV', '../../../Work/videos/RealData/Tim.MOV']
-    feature_csvs=['../../Work/videos/RealData/Chongyan_features.csv', '../../Work/videos/RealData/Ershad_features.csv', '../../Work/videos/RealData/Tim_features.csv']
+    feature_csvs=['../../Work/videos/RealData/Chongyan_features.csv', \
+                '../../Work/videos/RealData/Ershad_features.csv', \
+                '../../Work/videos/RealData/Tim_features.csv', \
+                '../../Work/videos/RealData/Subject_1_features.csv', \
+                '../../Work/videos/RealData/Subject_2_features.csv', \
+                '../../Work/videos/RealData/Subject_3_features.csv', \
+                '../../Work/videos/RealData/Subject_5_features.csv', \
+                '../../Work/videos/RealData/Subject_6_features.csv']
 
-    test, train = extract_test_train(feature_csvs, 0)
-    print(test.shape)
-    print(train.shape)
-    
-    combined = train.append(test)
-    combined.to_csv('feature.csv',index=False)
+    overall_accuracy = pd.DataFrame(0, index=np.arange(6), columns=['RF', 'MLP'])
 
-    print('r_wrist_velocity_y')
-    print(max(abs(combined[combined['label'] == 'steering']['r_wrist_velocity_y'])))
-    print(max(abs(combined[combined['label'] == 'texting']['r_wrist_velocity_y'])))
-    print(max(abs(combined[combined['label'] == 'calling_left']['r_wrist_velocity_y'])))
-    print(max(abs(combined[combined['label'] == 'calling_right']['r_wrist_velocity_y'])))
-    print(max(abs(combined[combined['label'] == 'reading']['r_wrist_velocity_y'])))
-    print(max(abs(combined[combined['label'] == 'eating_to_mouth']['r_wrist_velocity_y'])))
-    print(max(abs(combined[combined['label'] == 'eating_to_lap']['r_wrist_velocity_y'])))
+    for person_id in range(0,6):
+        test, train = extract_test_train(feature_csvs, person_id)
+        print(test.shape)
+        print(train.shape)
 
-    # print('r_wrist_velocity_x')
-    # print(max(abs(combined[combined['label'] == 'steering']['r_wrist_velocity_x'])))
-    # print(max(abs(combined[combined['label'] == 'texting']['r_wrist_velocity_x'])))
-    # print(max(abs(combined[combined['label'] == 'calling_left']['r_wrist_velocity_x'])))
-    # print(max(abs(combined[combined['label'] == 'calling_right']['r_wrist_velocity_x'])))
-    # print(max(abs(combined[combined['label'] == 'reading']['r_wrist_velocity_x'])))
-    # print(max(abs(combined[combined['label'] == 'eating_to_mouth']['r_wrist_velocity_x'])))
-    # print(max(abs(combined[combined['label'] == 'eating_to_lap']['r_wrist_velocity_x'])))
+        train = data_augment_eating(train, 140)
+        test = data_augment_eating(test, 35)
+        
+        combined = train.append(test)
+        combined.to_csv('feature.csv',index=False)
 
-    print('lr_wrist_dist')
-    print((abs(combined[combined['label'] == 'steering']['lr_wrist_dist']).mean()))
-    print((abs(combined[combined['label'] == 'texting']['lr_wrist_dist']).mean()))
-    print((abs(combined[combined['label'] == 'calling_left']['lr_wrist_dist']).mean()))
-    print((abs(combined[combined['label'] == 'calling_right']['lr_wrist_dist']).mean()))
-    print((abs(combined[combined['label'] == 'reading']['lr_wrist_dist']).mean()))
-    print((abs(combined[combined['label'] == 'eating_to_mouth']['lr_wrist_dist']).mean()))
-    print((abs(combined[combined['label'] == 'eating_to_lap']['lr_wrist_dist']).mean()))
+        # print('r_wrist_velocity_y')
+        # print(max(abs(combined[combined['label'] == 'steering']['r_wrist_velocity_y'])))
+        # print(max(abs(combined[combined['label'] == 'texting']['r_wrist_velocity_y'])))
+        # print(max(abs(combined[combined['label'] == 'calling_left']['r_wrist_velocity_y'])))
+        # print(max(abs(combined[combined['label'] == 'calling_right']['r_wrist_velocity_y'])))
+        # print(max(abs(combined[combined['label'] == 'reading']['r_wrist_velocity_y'])))
+        # print(max(abs(combined[combined['label'] == 'eating_to_mouth']['r_wrist_velocity_y'])))
+        # print(max(abs(combined[combined['label'] == 'eating_to_lap']['r_wrist_velocity_y'])))
 
-    start_feature = test.columns[0]
-    end_feature = test.columns[-2]
-    # all_inputdata = np.array(combined.loc[:, start_feature:end_feature])
-    # target = np.array(combined.loc[:, 'label'])
-    # Classifications_CV(all_inputdata,target)
-    Classifications_LOO(train.loc[:, start_feature:end_feature],train.loc[:, 'label'],
-        test.loc[:, start_feature:end_feature],test.loc[:, 'label'])
+        # print('lr_wrist_dist')
+        # print((abs(combined[combined['label'] == 'steering']['lr_wrist_dist']).mean()))
+        # print((abs(combined[combined['label'] == 'texting']['lr_wrist_dist']).mean()))
+        # print((abs(combined[combined['label'] == 'calling_left']['lr_wrist_dist']).mean()))
+        # print((abs(combined[combined['label'] == 'calling_right']['lr_wrist_dist']).mean()))
+        # print((abs(combined[combined['label'] == 'reading']['lr_wrist_dist']).mean()))
+        # print((abs(combined[combined['label'] == 'eating_to_mouth']['lr_wrist_dist']).mean()))
+        # print((abs(combined[combined['label'] == 'eating_to_lap']['lr_wrist_dist']).mean()))
+
+        # train = train[(train['label'] == 'eating_to_mouth') | (train['label'] == 'eating_to_lap')]
+        # test = test[(test['label'] == 'eating_to_mouth') | (test['label'] == 'eating_to_lap')]
+        # train = train[['lr_wrist_dist', 'label']]
+        # test = test[['lr_wrist_dist', 'label']]
+
+        start_feature = test.columns[0]
+        end_feature = test.columns[-2]
+        # all_inputdata = np.array(combined.loc[:, start_feature:end_feature])
+        # target = np.array(combined.loc[:, 'label'])
+        # Classifications_CV(all_inputdata,target)
+        rf_acc, mlp_acc = Classifications_LOO(train.loc[:, start_feature:end_feature],train.loc[:, 'label'],
+            test.loc[:, start_feature:end_feature],test.loc[:, 'label'])
+
+        overall_accuracy.loc[person_id] = [rf_acc, mlp_acc]
+
+    print(overall_accuracy)